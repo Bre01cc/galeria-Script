4d6ad8effc9b12f89a9cf70d8913afa0c3b18800
BRENO OLIVEIRA ASSIS REIS <25133656@dsn127m1241954.cfp127aluno.local>
2025-08-11 11:23:37 -0300

    projeto em andamento

--- img/main.js
+++ img/main.js
@@ -1,32 +1,73 @@
 /*
-curso de otimização de html e css */
+curso de otimização de html e css
+JSon notação de objetodo javaScript
+array de objetos 
+*/
 
 'use script'
 const imagens = [
-    "./imagem1.png",
-    "./imagem2.jpg",
-    "./imagem3.png",
-    "./imagem4.png",
-    "./imagem5.png",
-    "./imagem6.png",
-    "./imagem7.png",
-    "./imagem8.png"
+  {//chave e valor 
+    nome:'Imagem',
+    url:'./imagem1.png'
+  },
+  {
+    nome:'Imagem',
+    url:'./imagem2.jpg'
+
+  },
+  {
+    nome:'Imagem',
+    url:'./imagem3.png'
+
+  },
+  {
+    nome:'Imagem',
+    url:'./imagem4.png'
+
+  }, {
+    nome:'Imagem',
+    url:'./imagem5.png'
+
+  }, {
+    nome:'Imagem',
+    url:'./imagem6.png'
+
+  }, {
+    nome:'Imagem',
+    url:'./imagem7.png'
+
+  }, {
+    nome:'Imagem',
+    url:'./imagem8.png'
+
+  }
 ]
+function crearImagem(srcImagem){
+    const galeria = document.getElementById('galeria')
+    const imagem = document.createElement('img')
+    imagem.src = srcImagem.url
+    galeria.appendChild(imagem)
+
+
+}
 
 function carregarImagens() {
-    const galeria = document.getElementById('galeria')
+    
     //galeria.textContent("texto")
     //javaScript é dinamicamente tipado, logo o seu tipo varia de acordo com o dado inserido nela
     
     //atribuio a um indice do array imagens
-    let contador =0
-    while(contador<=7){
-        const imagem = document.createElement('img')
-        imagem.src = imagens[contador]
-        //Adicionando um filho para galeria
-        galeria.appendChild(imagem)
-        contador++
-   }
+//     let contador =0
+//     const limite= imagens.length-1
+//     while(contador<=limite){
+//         const imagem = document.createElement('img')
+//         imagem.src = imagens[contador]
+//         //Adicionando um filho para galeria
+//         galeria.appendChild(imagem)
+//         contador++
+//    }
+
+imagens.forEach(crearImagem)
     
 }
 carregarImagens()
